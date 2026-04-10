33e53b681ec0da89f6d310493cf2d2c01de7b705
Ricardo <code@ricardoandaluz.com>
2026-04-10 12:13:00 +0100

    Implement retract_promise method in PromiseRepository for promise retraction and event handling

--- Promises Service/repository.py
+++ Promises Service/repository.py
@@ -37,4 +37,31 @@ class PromiseRepository:
 
         #Commit both at the same time
         await database.commit()
+    
+    async def retract_promise(self, database: AsyncSession, promise: Promise):
+        get_promise = await database.execute(select(Promise).where(Promise.id == promise.id))
+        promise_from_db = get_promise.scalar_one_or_none()
+        
+        if promise_from_db == None:
+            return None
+        
+        # Live SQL Alchemy instance, allows to set attributes directly
+        promise_from_db.status = PromiseStatus.RETRACTING
+        
+        retract_promise_payload = build_promise_retracted_payload(
+            promise_id=promise_from_db.id,
+        )
+        
+        outbox_event = OutboxEvent(
+            event_type= PROMISE_RETRACTED,
+            aggregate_id= promise.id,
+            payload= retract_promise_payload
+        )
+        
+        database.add(outbox_event)
+        
+        await database.commit()
+        
+        return promise_from_db
+        
         
